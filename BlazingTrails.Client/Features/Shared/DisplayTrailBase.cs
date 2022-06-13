9e4dcf7a4fc5a520cef4b291c3f4ae3144fffb1c
Zeynel Şahin <zynl.shn@hotmail.com>
2022-06-13 23:19:41 +0300

    Css düzenlemeleri

---
+++ BlazingTrails.Client/Features/Shared/DisplayTrailBase.cs
@@ -0,0 +1,9 @@
+﻿using BlazingTrails.Client.Features.Home;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingTrails.Client.Features.Shared;
+
+public class DisplayTrailBase: ComponentBase
+{
+     [Parameter] public IEnumerable<Trail> Trails { get; set; }
+}
